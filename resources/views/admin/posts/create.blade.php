678d8893d8fcf87a191f6f4723bd80b993b10da1
Mikele91 <michele.amoruso91.8@gmail.com>
2021-11-15 18:25:41 +0100

    finito

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,14 +15,14 @@
                         {{-- @method("POST") --}}
                     <div class="form-group">
                         <label for="title">Titolo</label>
-                        <input type="text" name ="title" class="form-control @error('title') is-invalid @enderror" id="title"  placeholder="inserisci il titolo">
+                        <input type="text" name ="title" class="form-control @error('title') is-invalid @enderror" id="title"  placeholder="inserisci il titolo" value="{{old("title")}}">
                         @error('title')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
                         </div>
                         <div class="form-group">
                             <label for="content">Contenuto</label>
-                            <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" cols="30" rows="10" placeholder="inserisci il contenuto"></textarea>
+                            <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" cols="30" rows="10" placeholder="inserisci il contenuto">{{old("content")}}</textarea>
                             @error('content')
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
